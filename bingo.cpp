0e7f8ac238215447c79cf180c6e506a4c6952456
Scott Chen <rightson@gmail.com>
2015-02-09 16:55:21 +0800

    Update bingo.cpp

--- bingo.cpp
+++ bingo.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <fstream>
 #include <map>
 #include <vector>
+#include <set>
+#include <string>
 #include <string.h>
 using namespace std;
 
@@ -21,10 +24,11 @@ int TICKETS [][SIZE][SIZE] = {
 
 class Ticket {
 public:
-	Ticket (int nums [][SIZE]);
+	Ticket (string name, int nums [][SIZE]);
 	int checkBingo (int num);
 
 	void printOpened ();
+    string name;
 
 private:
 	std::map < int, std::pair<int, int> > _numToIndex;
@@ -32,9 +36,11 @@ private:
 	int _numLines;
 };
 
-Ticket :: Ticket (int nums [][SIZE]) : _numLines (0)
+Ticket :: Ticket (string name, int nums [][SIZE]) : _numLines (0)
 {
+    this->name = name;
 	memset (_openedNums, 0, SIZE * SIZE);
+    _openedNums[2][2] = true;
 	for (int y = 0; y < SIZE; y ++) {
 		for (int x = 0; x < SIZE; x ++) {
 			_numToIndex .insert (std::make_pair (nums [y] [x], std::make_pair (y, x)));
@@ -92,23 +98,81 @@ void Ticket :: printOpened ()
 	printf ("\n");
 }
 
+Ticket createTicket(string line) {
+	int nums[][SIZE] = {
+        {1, 2, 3, 4, 5},
+	    {6, 7, 8, 9, 10},
+	    {11, 12, 13, 14, 15},
+	    {16, 17, 18, 19, 20},
+	    {21, 22, 23, 24, 25}
+    };
+    Ticket ticket("name", nums);
+    return ticket;
+}
+
+void readTickets(string filename, vector <Ticket> &tickets) {
+    ifstream myfile (filename.c_str());
+    string line;
+    if (!myfile.is_open()) {
+        throw "cannot read ticket.txt file";
+    }
+    string key, dummy;
+    int num;
+    int nums[SIZE][SIZE]; 
+    int x = 0;
+    int y = 0;
+    while (myfile.peek() != EOF) {
+        myfile >> key >> dummy;
+        cout << key << endl;
+        x = 0;
+        for (int i=0; i<SIZE*SIZE; i++) {
+            myfile >> num;  
+            nums[x][y] = num;
+            //cout << x << " " << y << " " << nums[x][y] << endl;
+            y++;
+            if (y % 5 == 0) {
+                y = 0;
+                x++;
+            }
+        }
+        tickets.push_back(Ticket(key, nums));
+    }
+}
+
 int main () {
 
 	vector <Ticket> tickets;
-	for (int i = 0; i < NUM_TICKETS; i ++) {
-		Ticket ticket (TICKETS [i]);
-		tickets .push_back (ticket);
-	}
+    readTickets("input.txt", tickets);
+	//for (int i = 0; i < NUM_TICKETS; i ++) {
+	//	Ticket ticket (TICKETS [i]);
+	//	tickets .push_back (ticket);
+	//}
 
 	int num;
+    map<string, int> bingo;
+    set<int> opening;
 	while (cin >> num) {
+        opening.insert(num);
 		for (size_t i = 0; i < tickets .size (); i ++) {
 			int numLines = tickets [i] .checkBingo (num);
 			if (numLines > 0) {
-				printf ("[%d] %d lines\n", i + 1, numLines);
+				printf ("No. %s [%lu] %d lines\n", tickets[i].name.c_str(), i + 1, numLines);
 				tickets [i] .printOpened ();
 			}
+			if (numLines > 0) {
+                bingo[tickets[i].name] = numLines;
+			}
 		}
+        printf("Match\n");
+        for (map<string, int>::iterator it = bingo.begin(); it != bingo.end(); ++it) {
+		    printf ("Card=%s line=%d", it->first.c_str(), it->second);
+            printf("\n");
+        }
+        printf("Opening: ");
+        for (set<int>::iterator it = opening.begin(); it != opening.end(); ++it) {
+		    printf ("%d ", *it);
+        }
+        printf("\n");
 	}
 
 	return 0;
